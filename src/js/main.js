f095d60b6c9784165d42c2093880e39271c52c6d
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-01-27 11:18:46 -0600

    triangles

--- src/js/main.js
+++ src/js/main.js
@@ -157,6 +157,7 @@ var draw = (function () {
     },
 
     // RECTANGLE
+    //Draw a rectangle
     drawRect: function () {
       //Start by using random fill colors.
       ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -242,7 +243,7 @@ document.getElementById('btnPath').addEventListener('click', function () {
   draw.setShape('path');
 }, false);
 
-document.getElementById('#btnTriangle').addEventListener('click', function () {
+document.getElementById('btnTriangle').addEventListener('click', function () {
   draw.setShape('triangle');
 }, false);
 
