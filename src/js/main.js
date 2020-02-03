0266c7ba6c70742c3dbb9012d491b05d7ed84dd5
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-02-03 15:03:15 -0600

    added color selectors

--- src/js/main.js
+++ src/js/main.js
@@ -3,179 +3,228 @@ var draw = (function () {
   //Get the height and width of the main we will use this set canvas to the full
   //size of the main element.
   var mWidth = document.querySelector('main').offsetWidth,
-    mHeight = document.querySelector('main').offsetHeight,
+      mHeight = document.querySelector('main').offsetHeight,
+      // mWidthx = $('main').offsetWidth,
+      // mHeightx = $('main').offsetHeight,
 
-    //Create the canvas
-    canvas = document.createElement("canvas"),
+      //Create the canvas
+      canvas = document.createElement("canvas"),
 
-    //Create the context
-    ctx = canvas.getContext("2d"),
+      //Create the context
+      ctx = canvas.getContext("2d"),
 
-    //Create the initial bounding rectangle
-    rect = canvas.getBoundingClientRect(),
+      //Create the initial bounding rectangle
+      rect = canvas.getBoundingClientRect(),
 
-    //current x,y position
-    x = 0,
-    y = 0,
+      //current x,y position
+      x = 0,
+      y = 0,
 
-    //starting x,y
-    x1 = 0,
-    y1 = 0,
+      //starting x,y
+      x1 = 0,
+      y1 = 0,
 
-    //ending x,y
-    x2 = 0,
-    y2 = 0,
+      //ending x,y
+      x2 = 0,
+      y2 = 0,
 
-    //Tracks the last x,y state
-    lx = false,
-    ly = false,
+      //Tracks the last x,y state
+      lx = false,
+      ly = false,
 
-    //What shape are we drawing?
-    shape = '',
+      //What shape are we drawing?
+      shape = '',
 
-    //Are we drawimg a path?
-    isDrawing = false;
+      //Are we drawimg a path?
+      isDrawing = false;
 
+  //stroke color
+  stroke = '';
+
+  //fill color
+  fill = '';
+
+
+
+  // all matched (slightly)
   return {
+      setIsDrawing: function (bool) {
+          isDrawing = bool;
+      },
 
-    //Set the x,y coords based on current event data
-    setXY: function (evt) {
+      getIsDrawing: function () {
+          return isDrawing;
+      },
 
-      //Track last x,y position before setting the current posiiton.
-      lx = x;
-      ly = y;
+      getShape: function () {
+          return shape;
+      },
 
-      //Set the current x,y position
-      x = (evt.clientX - rect.left) - canvas.offsetLeft;
-      y = (evt.clientY - rect.top) - canvas.offsetTop;
-    },
+      //Sets the shape to be drawn
+      setShape: function (shp) {
+          shape = shp;
+      },
 
-    //Write the x,y coods to the target div
-    writeXY: function () {
-      document.getElementById('trackX').innerHTML = 'X: ' + x;
-      document.getElementById('trackY').innerHTML = 'Y: ' + y;
-    },
+      //Set a random color
+      randColor: function () {
+          return '#' + Math.floor(Math.random() * 16777215).toString(16);
+      },
 
-    //Set the x1,y1
-    setStart: function () {
-      x1 = x;
-      y1 = y;
-    },
+      //A setter for stroke
+      setStrokeColor: function (color) {
+          stroke = color;
+      },
 
-    //Set the x2,y2
-    setEnd: function () {
-      x2 = x;
-      y2 = y;
-    },
+      //A setter for fill
+      setFillColor: function (color) {
+          fill = color;
+      },
 
-    //Sets the shape to be drawn
-    setShape: function (shp) {
-      shape = shp;
-    },
+      //A getter for stroke
+      getStrokeColor: function () {
 
-    getShape: function () {
-      return shape;
-    },
+          if (stroke.length > 6) {
+              return stroke;
+          }
 
-    setIsDrawing: function (bool) {
-      isDrawing = bool;
-    },
+          return this.randColor();
+      },
 
-    getIsDrawing: function () {
-      return isDrawing;
-    },
+      //A getter for fill
+      getFillColor: function () {
 
-    //Draws the selected shape
-    draw: function () {
-      ctx.restore();
-      if (shape === 'rectangle') {
-        this.drawRect();
-      } else if (shape === 'line') {
-        this.drawLine();
-      } else if (shape === 'path') {
-        this.drawPath();
-      } else if (shape === 'circle') {
-        this.drawCircle();
+          if (fill.length > 6) {
+              return fill;
+          }
+
+          return this.randColor();
+      },
+
+      //Set the x1,y1
+      setStart: function () {
+          x1 = x;
+          y1 = y;
+      },
+
+      //Set the x2,y2
+      setEnd: function () {
+          x2 = x;
+          y2 = y;
+      },
+
+      //Set the x,y coords based on current event data
+      setXY: function (evt) {
+
+          //Track last x,y position before setting the current posiiton.
+          lx = x;
+          ly = y;
+
+          //Set the current x,y position
+          x = (evt.clientX - rect.left) - canvas.offsetLeft;
+          y = (evt.clientY - rect.top) - canvas.offsetTop;
+      },
+
+      //Write the x,y coods to the target div
+      writeXY: function () {
+          document.getElementById('trackX').innerHTML = 'X: ' + x;
+          document.getElementById('trackY').innerHTML = 'Y: ' + y;
+      },
+
+      //Draws the selected shape
+      draw: function () {
+          ctx.restore();
+          if (shape === 'rectangle') {
+              this.drawRect();
+          } else if (shape === 'line') {
+              this.drawLine();
+          } else if (shape === 'path') {
+              this.drawPath();
+          } else if (shape === 'circle') {
+              this.drawCircle();
+          }
+          //triangle
+          else if (shape === 'triangle') {
+              this.drawTriangle();
+          } else {
+              alert('Please choose a shape');
+          }
+          ctx.save();
+      },
+
+      // CIRCLE
+      drawCircle: function () {
+          // ctx.strokeStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+          // ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+          ctx.strokeStyle = this.getStrokeColor();
+          ctx.fillStyle = this.getFillColor();
+
+          let a = (x1 - x2)
+          let b = (y1 - y2)
+          let radius = Math.sqrt(a * a + b * b);
+
+          ctx.beginPath();
+          ctx.arc(x1, y1, radius, 0, 2 * Math.PI);
+          ctx.stroke();
+          ctx.fill();
+      },
+
+      // LINE
+      drawLine: function () {
+          //Start by using random fill colors.
+          // ctx.strokeStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+          ctx.strokeStyle = this.getStrokeColor();
+          ctx.beginPath();
+          ctx.moveTo(x1, y1);
+          ctx.lineTo(x2, y2);
+          ctx.stroke();
+      },
+
+      // TRIANGLE
+      drawTriangle: function () {
+          //Start by using random fill colors.
+          // ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+          ctx.fillStyle = this.getFillColor();
+          ctx.strokeStyle = this.getStrokeColor();
+          //ctx.fillRect(x1, y1, (x1 - y2), (x2 - y2));
+          ctx.beginPath();
+          ctx.moveTo(x1, y1);
+          ctx.lineTo(x1, y2);
+          ctx.lineTo(x2, y2);
+          ctx.lineTo(x1, y1);
+          ctx.stroke();
+          ctx.fill();
+      },
+
+      // PATH
+      drawPath: function () {
+          ctx.strokeStyle = this.getStrokeColor();
+          ctx.beginPath();
+          ctx.moveTo(lx, ly);
+          ctx.lineTo(x, y);
+          ctx.stroke();
+      },
+
+      // RECTANGLE
+      //Draw a rectangle
+      drawRect: function () {
+          //Start by using random fill colors.
+          // ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+          ctx.fillStyle = this.getFillColor();
+          ctx.strokeStyle = this.getStrokeColor();
+          ctx.fillRect(x1, y1, (x2 - x1), (y2 - y1));
+      },
+
+      getCanvas: function () {
+          return canvas;
+      },
+
+      //Initialize the object, this must be called before anything else
+      init: function () {
+          canvas.width = mWidth;
+          canvas.height = mHeight;
+          document.querySelector('main').appendChild(canvas);
+
       }
-      //triangle
-      else if (shape === 'triangle') {
-        this.drawTriangle();
-      } else {
-        alert('Please choose a shape');
-      }
-      ctx.save();
-    },
-
-    // CIRCLE
-    drawCircle: function () {
-
-      ctx.strokeStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      let a = (x1 - x2)
-      let b = (y1 - y2)
-      let radius = Math.sqrt(a * a + b * b);
-
-      ctx.beginPath();
-      ctx.arc(x1, y1, radius, 0, 2 * Math.PI);
-      ctx.stroke();
-      ctx.fill();
-    },
-
-    // LINE
-    drawLine: function () {
-      //Start by using random fill colors.
-      ctx.strokeStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      ctx.beginPath();
-      ctx.moveTo(x1, y1);
-      ctx.lineTo(x2, y2);
-      ctx.stroke();
-    },
-
-    // TRIANGLE
-    drawTriangle: function () {
-      //Start by using random fill colors.
-      ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      //ctx.fillRect(x1, y1, (x1 - y2), (x2 - y2));
-      ctx.beginPath();
-      ctx.moveTo(x1, y1);
-      ctx.lineTo(x1, y2);
-      ctx.lineTo(x2, y2);
-      ctx.lineTo(x1, y1);
-      ctx.stroke();
-      ctx.fill();
-    },
-
-    // PATH
-    drawPath: function () {
-      //console.log({x1:x,y1:y,x2:x2,y2:y2});
-      //Start by using random fill colors.
-      ctx.strokeStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      ctx.beginPath();
-      ctx.moveTo(lx, ly);
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    },
-
-    // RECTANGLE
-    //Draw a rectangle
-    drawRect: function () {
-      //Start by using random fill colors.
-      ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      ctx.fillRect(x1, y1, (x2 - x1), (y2 - y1));
-    },
-
-    getCanvas: function () {
-      return canvas;
-    },
-
-    //Initialize the object, this must be called before anything else
-    init: function () {
-      canvas.width = mWidth;
-      canvas.height = mHeight;
-      document.querySelector('main').appendChild(canvas);
-
-    }
   };
 
 })();
@@ -193,7 +242,7 @@ draw.getCanvas().addEventListener('mousemove', function (evt) {
   draw.setXY(evt);
   draw.writeXY();
   if (draw.getShape() == 'path' && draw.getIsDrawing() === true) {
-    draw.draw();
+      draw.draw();
   }
 }, false);
 
@@ -247,6 +296,22 @@ document.getElementById('btnPath').addEventListener('click', function () {
 document.getElementById('btnTriangle').addEventListener('click', function () {
   draw.setShape('triangle');
 }, false);
+
+document.getElementById('strokeColor').addEventListener('change', function(){
+  draw.setStrokeColor(document.getElementById('strokeColor').value);
+});
+
+document.getElementById('randStrokeColor').addEventListener('change', function(){
+  draw.setStrokeColor('');
+});
+
+document.getElementById('fillColor').addEventListener('change', function(){
+  draw.setFillColor(document.getElementById('fillColor').value);
+});
+
+document.getElementById('randFillColor').addEventListener('change', function(){
+  draw.setFillColor('');
+});
 
 //$('btnTriangle').click(function() { /* handle click event */ });
 //$('btnTriangle').mouseenter(function() {  /* handle click event */ });
